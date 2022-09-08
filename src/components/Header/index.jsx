ebe9d842ad357c8f066e0596e55407630d64deea
MagnusBittencourt <magnusbittencourt@gmail.com>
2022-09-08 14:50:02 -0300

    add atualizações do index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,16 +4,17 @@ export function Header(){
   return ( 
      <HeaderStyle>
       <section className="HeaderFontes">
-        <h2>uma seleção de produtos</h2>
-        <h1>especial para você</h1>
-        <h3>Todos os produtos desta lista foram selecionados a partir da sua navegação. 
+       <h2>uma seleção de produtos</h2>
+       <h1>especial para você</h1>
+       <h3>Todos os produtos desta lista foram selecionados a partir da sua navegação.
           Aproveite!</h3>
+       
       </section>    
       <section >
-        <button className="bnt"></button>
-        <button className="bnt"></button>
-        <button className="bnt"></button>
-        <button className="bnt"></button>
+        <button className="bnt">Conheça a Linx</button>
+        <button className="bnt">Ajude o algorítimo</button> 
+        <button className="bnt">Seus produtos</button>
+        <button className="bnt">compartilhe</button>
       </section>
      </HeaderStyle>
      ) 

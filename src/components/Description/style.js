bcda9568524b419d8428b658f1dd2cf2977a1548
MagnusBittencourt <magnusbittencourt@gmail.com>
2022-09-09 00:28:15 -0300

    add atualização no style de Description

--- src/components/Description/style.js
+++ src/components/Description/style.js
@@ -1,8 +1,28 @@
 import styled from "styled-components";
 
 export const DescriptionStyle = styled.section`
+width: 100%;
+height: 390px;
+color: #888888;
+display: flex;
+justify-content: center;
+.descriptionContainer{
+  
+  display: flex;
 
-
-
+}
+.boxDescription{
+width: 520px;
+height: 320px;
+text-align: left;
+display: flex;
+flex-direction: column;
+gap: 2rem;
+} 
+.Cadastro{
+  width: 340px;
+  height: 280px;
+  flex-direction: column;
+}
 
 `

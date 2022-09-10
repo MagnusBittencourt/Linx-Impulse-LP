61cd0a594fa9504ce61fc4a4d4540537e92d3e52
MagnusBittencourt <magnusbittencourt@gmail.com>
2022-09-10 05:55:17 -0300

    add o style.js completo

---
+++ src/components/Divider/style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const DividerStyle = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1.5rem;
+  margin-top: 5rem;
+  p{
+    font-size: 25px;
+    white-space: nowrap;
+    color: #707070;
+  }
+  .line{
+    width: 400px;
+    height: 1px;
+    background-color: #707070;
+  }
+`;

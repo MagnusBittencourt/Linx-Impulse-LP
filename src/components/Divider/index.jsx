051d4c61b39544a8d3396a5d3eb1300606036623
MagnusBittencourt <magnusbittencourt@gmail.com>
2022-09-10 05:54:51 -0300

    add o index.jsx completo

---
+++ src/components/Divider/index.jsx
@@ -0,0 +1,11 @@
+import { DividerStyle } from './style';
+
+export function Divider({ children }) {
+  return (
+    <DividerStyle>
+      <span className="line" />
+      <p>{children}</p>
+      <span className="line" />
+    </DividerStyle>
+  );
+}

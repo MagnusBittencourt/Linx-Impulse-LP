2f61f5b87fde3c7327ec1bd6d6bb494fb3cccfd7
MagnusBittencourt <magnusbittencourt@gmail.com>
2022-09-10 05:55:54 -0300

    add modificação na home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,11 @@
 import { Description } from "../../components/Description"
 import { Header } from "../../components/Header"
+import { Divider } from "../../components/Divider"
 export function Home (){
   return (<div>
    <Header/>
     <Description/>
+    <Divider>Sua seleção especial</Divider>
    </div>
   )
 }

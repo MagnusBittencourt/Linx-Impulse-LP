65f0018d5422da3c8efbfc89b8adbc74ea4df99d
MagnusBittencourt <magnusbittencourt@gmail.com>
2022-09-11 17:58:03 -0300

    add a home

---
+++ src/components/ProductCard/style.js
@@ -0,0 +1,92 @@
+import styled from "styled-components";
+
+export const CardsStyle = styled.div`
+
+
+.teste2{
+display: flex;
+justify-content: center;
+align-items: center;
+
+}
+.teste{
+ 
+width: 931px;
+height: 740px;
+display: grid;
+grid-template-columns: repeat(4, 200px);
+gap: 3rem;
+margin: 2rem;
+align-items: center;
+justify-content: center;
+}
+
+.FotoCards{
+width: 200px;
+height: 350px;
+}
+
+.info{
+width: 200px;
+height: 60px;
+}
+
+.img{
+width: 200px;
+height: 150px;
+}
+
+.valor-antigo{
+text-align: left;
+font: normal normal normal 12px/16px Helvetica Neue;
+letter-spacing: 0px;
+color: #888888;
+opacity: 1;
+margin-top: 20px;
+}
+
+.promoção{
+margin-top: 10px;
+text-align: left;
+font: normal normal bold 16px/16px Helvetica Neue;
+letter-spacing: 0px;
+color: #888888;
+opacity: 1;
+}
+
+.dividindo{
+margin-top: 10px;
+font: normal normal normal 12px/16px Helvetica Neue;
+letter-spacing: 0px;
+color: #888888;
+opacity: 1; 
+}
+
+.bnt{
+  margin-top: 10px;
+  width: 200px;
+height: 30px;
+  background: #FFFFFF 0% 0% no-repeat padding-box;
+border: 1px solid #707070;
+border-radius: 4px;
+opacity: 1;
+}
+
+
+
+.aaaa{
+  margin-top: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.mais-produtos{
+width: 260px;
+height: 39px;
+background: #FFFFFF 0% 0% no-repeat padding-box;
+border: 1px solid #707070;
+border-radius: 4px;
+opacity: 1;
+
+}
+`
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,14 @@
 import { Description } from "../../components/Description"
 import { Header } from "../../components/Header"
 import { Divider } from "../../components/Divider"
+import { Cards } from "../../components/ProductCard"
+Cards
 export function Home (){
   return (<div>
    <Header/>
     <Description/>
     <Divider>Sua seleção especial</Divider>
+    <Cards/>
    </div>
   )
 }

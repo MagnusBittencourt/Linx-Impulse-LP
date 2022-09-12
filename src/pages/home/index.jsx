d90bd0b0d2a05786e9f2a17056634f43657ac5b7
MagnusBittencourt <magnusbittencourt@gmail.com>
2022-09-11 21:16:00 -0300

    add mais conteúdo a home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,6 +2,8 @@ import { Description } from "../../components/Description"
 import { Header } from "../../components/Header"
 import { Divider } from "../../components/Divider"
 import { Cards } from "../../components/ProductCard"
+import { Compartilhe } from "../../components/Compartilhe"
+Compartilhe
 Cards
 export function Home (){
   return (<div>
@@ -9,6 +11,8 @@ export function Home (){
     <Description/>
     <Divider>Sua seleção especial</Divider>
     <Cards/>
+    <Divider>Compartilhe a novidade</Divider>
+    <Compartilhe/>
    </div>
   )
 }

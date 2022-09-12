9898f157532bb7100e5c4c6a6567227ff6bdf812
MagnusBittencourt <magnusbittencourt@gmail.com>
2022-09-11 23:50:42 -0300

    finalizando o compartilhe.jsx

--- src/components/Compartilhe/index.jsx
+++ src/components/Compartilhe/index.jsx
@@ -1,34 +1,28 @@
 import { CompartilheStyle } from "./style";
 
 export function Compartilhe() {
-  return(
-<CompartilheStyle>
-
-<div className="novidade">
-Quer que seus amigos também ganhem a lista personalizada deles? Preencha agora!
-</div>
-
-<div className="teste2">
-  <div className="teste">
-    <div className="inputContainer">
-     <label htmlFor="nome">Nome do seu amigo:</label>
-     <input type="" id="nome" required />
-     </div>
-  
-      <div className="inputContainer">
-      <label htmlFor="email">E-mail</label>
-      <input type="" id="email" required />
+  return (
+    <CompartilheStyle>
+      <div className="novidade">
+        Quer que seus amigos também ganhem a lista personalizada deles? Preencha
+        agora!
       </div>
-  </div>
-</div>
 
+      <div className="container">
+        <div className="containerFilho">
+          <div className="inputContainer">
+            <label htmlFor="nome">Nome do seu amigo:</label>
+            <input type="" id="nome" required />
+          </div>
 
-<button className="bnt">Enviar agora</button>
+          <div className="inputContainer">
+            <label htmlFor="email">E-mail</label>
+            <input type="" id="email" required />
+          </div>
+        </div>
+      </div>
 
-
-
-
-
-</CompartilheStyle>
-
-)}
+      <button className="bnt">Enviar agora</button>
+    </CompartilheStyle>
+  );
+}

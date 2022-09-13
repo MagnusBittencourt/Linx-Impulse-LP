ed3f5001471250c7461ee5ff5bf75a950504d238
MagnusBittencourt <magnusbittencourt@gmail.com>
2022-09-12 23:08:47 -0300

    teste

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './pages/Home/index'
+// import Home from './pages/Home/index'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <div><h1>teste</h1></div>
   </React.StrictMode>
 )
